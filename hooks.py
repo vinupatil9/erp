bd580a5d8a9aa85411cf6c6bdcda9d18b206e004
vinupatil9 <116336922+vinupatil9@users.noreply.github.com>
2022-12-14 11:22:13 +0530

    Update hooks.py

--- hooks.py
+++ hooks.py
@@ -9,7 +9,8 @@ app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+#app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+app_logo_url = "/assets/erpnext/images/logo_ops.svg"
 required_apps = ["payments"]
 
 
